0c3080774c7f1bebd6b720454e7922d9e929bc58
Samim Mirhosseini <samim.mirhoseini@gmail.com>
2017-12-25 00:45:34 -0500

    Adding install step to Baker formula

--- Formula/baker.rb
+++ Formula/baker.rb
@@ -16,6 +16,7 @@ class Baker < Formula
     #                       "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
     # system "make", "install" # if this fails, try separate make/make install steps
+    bin.install "weather"
   end
 
   # test do
